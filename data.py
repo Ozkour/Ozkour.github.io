5c5b0ac0c2a0c0278aa0ca5094899ad6e665dc61
Nicolas <nicolas.kerman.etu@univ-lille.fr>
2023-06-01 16:35:22 +0200

    first stacked chart and horizontal chart

--- data.py
+++ data.py
@@ -6,10 +6,10 @@ data2017 = list()
 data2018 = list()
 data2019 = list()
 
-dicNA = {}
+counts = {}
 dicEU = {}
 # open file for reading
-with open('chi-demographics-2016-2022.csv') as csvDataFile:
+with open('chi-data.csv') as csvDataFile:
     # read file as csv file 
     csvReader = csv.reader(csvDataFile)
     # for every row, print the row
@@ -23,14 +23,21 @@ with open('chi-demographics-2016-2022.csv') as csvDataFile:
         if row[3] != "":
             data2019.append(row[3])
 
-datas = data2016[2:] + data2017[2:] + data2018[2:]
-for data in datas:
-    if data not in dicNA:
-        dicNA[data] = math.ceil(datas.count(data)/3)
 
-for data in data2019[2:]:
-    if data not in dicEU:
-        dicEU[data] = data2019.count(data)
+datas = data2016[1:] + data2017[1:] + data2018[1:] + data2019[1:]
+for data in datas:
+    if data not in counts:
+        counts[data] = {2016: data2016.count(data), 2017: data2017.count(data), 2018: data2018.count(data), 2019: data2019.count(data)}
+
+with open('chi-countries-counts.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',')
+    writer.writerow(['Country', 'y2016', 'y2017', 'y2018', 'y2019' ])
+    for data in counts:
+        writer.writerow([data, counts[data][2016], counts[data][2017], counts[data][2018], counts[data][2019]])
+
+# for data in data2019[2:]:
+#     if data not in dicEU:
+#         dicEU[data] = data2019.count(data)
 # print("NA")
 # print(sum(dicNA.values()))
 # print({k: v for k, v in sorted(dicNA.items(), key=lambda item: item[1])})
@@ -38,15 +45,15 @@ for data in data2019[2:]:
 # print(sum(dicEU.values()))
 # print({k: v for k, v in sorted(dicEU.items(), key=lambda item: item[1])})
 
-def convert(deg):
-    return deg*math.pi/180
+# def convert(deg):
+#     return deg*math.pi/180
 
-phi1 = convert(33.9425222)
-phi2 = convert(49.00972222)
-lam1 = convert(-118.4071611)
-lam2 = convert(2.54861111)
-d = 2*6371.009*math.asin(math.sqrt(math.sin((phi2-phi1)/2)**2+math.cos(phi1)*math.cos(phi2)*math.sin((lam2-lam1)/2)**2))
-print(d)
+# phi1 = convert(33.9425222)
+# phi2 = convert(49.00972222)
+# lam1 = convert(-118.4071611)
+# lam2 = convert(2.54861111)
+# d = 2*6371.009*math.asin(math.sqrt(math.sin((phi2-phi1)/2)**2+math.cos(phi1)*math.cos(phi2)*math.sin((lam2-lam1)/2)**2))
+# print(d)
 
 
-print(phi1,phi2,lam1,lam2)
+# print(phi1,phi2,lam1,lam2)
