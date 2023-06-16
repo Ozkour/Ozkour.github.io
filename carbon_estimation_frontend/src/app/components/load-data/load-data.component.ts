e3718999d6002ebf38c791b3122de9f4ac282a9a
Nicolas <nicolas.kerman.etu@univ-lille.fr>
2023-06-16 17:02:28 +0200

    debut estimation temps, cas USA fait

--- carbon_estimation_frontend/src/app/components/load-data/load-data.component.ts
+++ carbon_estimation_frontend/src/app/components/load-data/load-data.component.ts
@@ -12,6 +12,7 @@ airports: Map<string, string> | any = new Map();
 to: string = "";
 toLocationTemplate: boolean = true;
 fromLocationTemplate: boolean = true;
+dateTemplate: boolean = true;
 destinations: any[] = [];
 toLocation: any[] = [];
 departureDateTemplate: boolean = false
@@ -20,12 +21,24 @@ origins: Map<string, string> | any = new Map();
 defaultOrigins: string[][] = [];
 airportsLoc: Map<string, Map<string, string>> = new Map();
 CO2est: Map<string, number> | any = new Map();
+timeEst: any = new Map();
 
 constructor() { 
   d3.csv('../assets/chi-countries-counts-test.csv').then(data =>{
     data.forEach((d: any) => {
-      this.origins.set(d['Country'], d['y2019']);
       this.defaultOrigins.push([d['Country'],d['y2019']]);
+      //case USA
+      if(d['Country'] == 'United States'){
+        this.origins.set('USA1', d['y2019']/3);
+        this.origins.set('USA2', d['y2019']/3);
+        this.origins.set('USA3', d['y2019']/3);
+        this.defaultOrigins.push(['USA1',""]);
+        this.defaultOrigins.push(['USA2',""]);
+        this.defaultOrigins.push(['USA3',""]);
+      }
+      else{
+        this.origins.set(d['Country'], d['y2019']);
+      }
     });
   });
 
@@ -82,11 +95,13 @@ constructor() {
 
     //compute CO2 estimation
     fetches.then(() => {
+      var cptFlights = new Map();
       var sum = 0;
       this.itineraries.forEach((itin: Map<string, any>) => { //for each origin-dest pair
         var origin = itin.get('origin');
         var dest = itin.get('dest');
         var flights = itin.get('flights');
+        var time = itin.get('time');
         flights.forEach((route: string[][]) =>{ //for each route
           route.forEach((stage: string[]) => { //for each stage
             var airportOrigin: any = this.airportsLoc.get(stage[0]);
@@ -102,10 +117,17 @@ constructor() {
         });
         // mean of routes for each origin-dest pair multiplied by the number of attendees
         var estimation = sum / flights.length * this.origins.get(this.airports.get(origin));
-        // add carbon footprint to destinations
-        this.CO2est.has(dest) ? this.CO2est.set(dest, this.CO2est.get(dest) + estimation) : this.CO2est.set(dest, estimation);
+        // add carbon footprint & time to destinations
+        //temps moyen doit etre par passager et pas par voyage
+        var countryDest = this.airports.get(dest);
+        this.CO2est.has(countryDest) ? this.CO2est.set(countryDest, this.CO2est.get(countryDest) + estimation) : this.CO2est.set(countryDest, estimation);
+        this.timeEst.has(countryDest) ? this.timeEst.set(countryDest, (this.timeEst.get(countryDest) * cptFlights.get(countryDest) + time) / (cptFlights.get(countryDest)+1)) : this.timeEst.set(countryDest, time);
+        cptFlights.has(countryDest) ? cptFlights.set(countryDest,cptFlights.get(countryDest) + 1) : cptFlights.set(countryDest,1);
         sum = 0;        
+        
       });
+      console.log('time :', this.timeEst);
+      console.log('CO2 :', this.CO2est);
     });
   }
 
@@ -114,7 +136,7 @@ constructor() {
     var currentDest = destinationData[destIndex];
     var origin = this.airports.get(currentOrigin[0]);
     var dest = this.airports.get(currentDest.country);
-    if(this.origins.get(currentOrigin[0]) != "0" && origin != dest){
+    if(this.origins.get(currentOrigin[0]) != "0" && origin != dest && currentOrigin[0] != 'United States'){
       await fetch(`http://localhost:5000/flights-search?originCode=${origin}&destinationCode=${dest}&dateOfDeparture=2023-07-25`)
         .then(response => response.json())
         .then(data => {
@@ -155,21 +177,38 @@ constructor() {
   addItineraries(flights: any, dest: any, origin: any){
     var routes: string[][][] = [];
     var stages: string[][] = [];
-    flights.forEach((flight: { itineraries: { segments: any; }[]; }) =>{
+    var totalTime = 0;
+    flights.forEach((flight: { itineraries: {duration: string; segments: any;}[]; }) =>{
       (flight.itineraries[0].segments).forEach((seg: { departure: { iataCode: any; }; arrival: { iataCode: any; }; }) =>{
         stages.push([seg.departure.iataCode, seg.arrival.iataCode]);
       });
+      var currentTime = flight.itineraries[0].duration;
+      if(currentTime.includes('H') && currentTime.includes('M')){// flight time > 1H
+        var arrTime = currentTime.substring(2, currentTime.length-1).split('H');
+        totalTime += parseInt(arrTime[0])*60 + parseInt(arrTime[1]);
+      }
+      else if(currentTime.includes('H')){// exactly a full hour
+        totalTime += parseInt(currentTime.substring(2, currentTime.length-1)) * 60;
+      }
+      else{// flight time < 1H
+        totalTime += parseInt(currentTime.substring(2, currentTime.length-1));
+      }
       routes.push(stages);
       stages = [];
       
     });
     // all stages of route are added
-    this.itineraries.push(new Map([['origin', origin],['dest', dest],['flights',routes]]));
+    this.itineraries.push(new Map([['origin', origin],['dest', dest],['flights',routes],['time',totalTime/flights.length]]));
   }
 
   // set number of attendees for a country
   changeVal(country: any, event: any){
     this.origins.set(country,event.target.value);
+    if(country == 'United States'){
+      this.origins.set('USA1', event.target.value/3);
+      this.origins.set('USA2', event.target.value/3);
+      this.origins.set('USA3', event.target.value/3);
+    }
   }
 
 }
