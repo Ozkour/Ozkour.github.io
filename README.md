aa746dcca976750751fd68d7ef535db12c3054b6
Nicolas <nicolas.kerman.etu@univ-lille.fr>
2023-06-19 17:18:10 +0200

    calendar, slider, time formula, todo in readme

--- README.md
+++ README.md
@@ -4,3 +4,16 @@ My github pages
 
 Pour lancer le backend aller dans carbon_estimation_app_backend et faire npm install et npm start
 Pour lancer le frontend aller dans carbon_estimation_frontend et faire npm install et ng serve --open
+
+Décrire les colonnes des csv
+Ce qui a été fait et TODO
+Build et déploiement
+author etc.
+
+TODO
+formule CO2
+déplacer formulaire destinations en haut
+slider order by nb desc ou alphabetique
+ajouter res a côté du nom de ville + graphiques bar chart, bubble chart 2D google charts
+bouger slider met a jour le reste sans avoir a relancer
+readme
--- carbon_estimation_frontend/src/app/components/load-data/load-data.component.ts
+++ carbon_estimation_frontend/src/app/components/load-data/load-data.component.ts
@@ -10,6 +10,8 @@ export class LoadDataComponent implements OnInit {
 
 airports: Map<string, string> | any = new Map();
 to: string = "";
+trip_start: string = "";
+real_date: string = "";
 toLocationTemplate: boolean = true;
 fromLocationTemplate: boolean = true;
 dateTemplate: boolean = true;
@@ -22,8 +24,13 @@ defaultOrigins: string[][] = [];
 airportsLoc: Map<string, Map<string, string>> = new Map();
 CO2est: Map<string, number> | any = new Map();
 timeEst: any = new Map();
+specialCases: string[] = ['United States', 'Canada', 'China'];
+today: string = "";
 
 constructor() { 
+  const date = new Date();
+  const month = (date.getMonth()+1) < 10 ? '0' + (date.getMonth()+1) : (date.getMonth()+1)
+  this.today = date.getFullYear() + '-' + month + '-' + date.getDate();
   d3.csv('../assets/chi-countries-counts-test.csv').then(data =>{
     data.forEach((d: any) => {
       this.defaultOrigins.push([d['Country'],d['y2019']]);
@@ -36,10 +43,30 @@ constructor() {
         this.defaultOrigins.push(['USA2',""]);
         this.defaultOrigins.push(['USA3',""]);
       }
-      else{
-        this.origins.set(d['Country'], d['y2019']);
+      else if(d['Country'] == 'Canada'){
+        this.origins.set('Cananda1', d['y2019']/3);
+        this.origins.set('Cananda2', d['y2019']/3);
+        this.origins.set('Cananda3', d['y2019']/3);
+        this.defaultOrigins.push(['Cananda1',""]);
+        this.defaultOrigins.push(['Cananda2',""]);
+        this.defaultOrigins.push(['Cananda3',""]);
       }
+      else if(d['Country'] == 'China'){
+        this.origins.set('China1', d['y2019']/3);
+        this.origins.set('China2', d['y2019']/3);
+        this.origins.set('China3', d['y2019']/3);
+        this.defaultOrigins.push(['China1',""]);
+        this.defaultOrigins.push(['China2',""]);
+        this.defaultOrigins.push(['China3',""]);
+      }
+      this.origins.set(d['Country'], d['y2019']);
     });
+    this.defaultOrigins.sort(function(a,b){
+      if(b[1] == "") return - parseInt(a[1]);
+      else if (a[1] == "") return parseInt(b[1]);
+      return parseInt(b[1]) - parseInt(a[1]);
+    });
+    
   });
 
   d3.csv('../assets/busiest_airports.csv').then((data) => {
@@ -82,6 +109,17 @@ constructor() {
     this.to = "";
   }
 
+  handleDate(){
+    const date = new Date();
+    const splitDate = this.trip_start.split('-');
+    if(parseInt(splitDate[1]) <= date.getMonth()+1){
+      this.real_date = (date.getFullYear()+1).toString()+'-'+splitDate[1]+'-'+splitDate[2];
+    }
+    else{
+      this.real_date = date.getFullYear().toString()+'-'+splitDate[1]+'-'+splitDate[2];
+    }
+  }
+
   deleteCity(location: any){
     this.destinations.splice(this.destinations.indexOf(location),1);
   }
@@ -115,19 +153,19 @@ constructor() {
             // sum += (latOrigin+longOrigin)*(latDest+longDest)/2;
           });
         });
+        var nbAttendees = parseInt(this.origins.get(this.airports.get(origin)));
         // mean of routes for each origin-dest pair multiplied by the number of attendees
-        var estimation = sum / flights.length * this.origins.get(this.airports.get(origin));
+        var estimation = sum / flights.length * nbAttendees;
         // add carbon footprint & time to destinations
-        //temps moyen doit etre par passager et pas par voyage
-        var countryDest = this.airports.get(dest);
-        this.CO2est.has(countryDest) ? this.CO2est.set(countryDest, this.CO2est.get(countryDest) + estimation) : this.CO2est.set(countryDest, estimation);
-        this.timeEst.has(countryDest) ? this.timeEst.set(countryDest, (this.timeEst.get(countryDest) * cptFlights.get(countryDest) + time) / (cptFlights.get(countryDest)+1)) : this.timeEst.set(countryDest, time);
-        cptFlights.has(countryDest) ? cptFlights.set(countryDest,cptFlights.get(countryDest) + 1) : cptFlights.set(countryDest,1);
+        this.CO2est.has(dest) ? this.CO2est.set(dest, this.CO2est.get(dest) + estimation) : this.CO2est.set(dest, estimation);
+        this.timeEst.has(dest) ? this.timeEst.set(dest, (this.timeEst.get(dest) * cptFlights.get(dest) + time*nbAttendees) / (cptFlights.get(dest)+nbAttendees)) : this.timeEst.set(dest, time);
+        cptFlights.has(dest) ? cptFlights.set(dest,cptFlights.get(dest) + nbAttendees) : cptFlights.set(dest,nbAttendees);
         sum = 0;        
-        
       });
-      console.log('time :', this.timeEst);
-      console.log('CO2 :', this.CO2est);
+        console.log('time :', this.timeEst);
+        console.log('CO2 :', this.CO2est);
+        console.log('cpt :',cptFlights);
+      
     });
   }
 
@@ -135,13 +173,13 @@ constructor() {
     var currentOrigin = originData[originIndex];
     var currentDest = destinationData[destIndex];
     var origin = this.airports.get(currentOrigin[0]);
-    var dest = this.airports.get(currentDest.country);
-    if(this.origins.get(currentOrigin[0]) != "0" && origin != dest && currentOrigin[0] != 'United States'){
+    var dest = this.specialCasesAirport(currentDest);
+    if(this.origins.get(currentOrigin[0]) != "0" && origin != dest && (!this.specialCases.includes(currentOrigin[0]))){
       await fetch(`http://localhost:5000/flights-search?originCode=${origin}&destinationCode=${dest}&dateOfDeparture=2023-07-25`)
         .then(response => response.json())
         .then(data => {
           if(data.data){
-            this.addItineraries(data.data, dest, origin);
+            this.addItineraries(data.data, currentDest.name, origin);
             setTimeout(() =>{this.nextFetch(originData, destinationData, originIndex, destIndex, resolve)},2000);
           }
           else{
@@ -157,6 +195,51 @@ constructor() {
     }
   }
 
+  specialCasesAirport(dest: { country: any; latitude: any; longitude: any; }){
+    if(['CA','US','CN'].includes(dest.country)){
+      var airport1 = this.airports.get(dest.country+'1');
+      var airport2 = this.airports.get(dest.country+'2');
+      var airport3 = this.airports.get(dest.country+'3');
+      var airport1Loc = this.airportsLoc.get(this.airports.get(dest.country+'1'));
+      var airport2Loc = this.airportsLoc.get(this.airports.get(dest.country+'2'));
+      var airport3Loc = this.airportsLoc.get(this.airports.get(dest.country+'3'));
+      var dist1 = this.haversineFormulaDistance(airport1Loc?.get('lat'),airport1Loc?.get('long'),dest.latitude,dest.longitude);
+      var dist2 = this.haversineFormulaDistance(airport2Loc?.get('lat'),airport2Loc?.get('long'),dest.latitude,dest.longitude);
+      var dist3 = this.haversineFormulaDistance(airport3Loc?.get('lat'),airport3Loc?.get('long'),dest.latitude,dest.longitude);
+      var minDist = Math.min(dist1,dist2,dist3);
+      if(minDist == dist1){
+        return airport1;
+      }
+      else if(minDist == dist2){
+        return airport2;
+      }
+      else{
+        return airport3;
+      }
+    }
+    else{
+      return this.airports.get(dest.country);
+    }
+  }
+
+  haversineFormulaDistance(lat1: any,lon1: any,lat2: any,lon2: any) {
+    var R = 6371; // Radius of the earth in km
+    var dLat = this.deg2rad(lat2-lat1);  // deg2rad below
+    var dLon = this.deg2rad(lon2-lon1); 
+    var a = 
+      Math.sin(dLat/2) * Math.sin(dLat/2) +
+      Math.cos(this.deg2rad(lat1)) * Math.cos(this.deg2rad(lat2)) * 
+      Math.sin(dLon/2) * Math.sin(dLon/2)
+      ; 
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    var d = R * c; // Distance in km
+    return d;
+  }
+  
+  deg2rad(deg: number) {
+    return deg * (Math.PI/180)
+  }
+
   // prepare index to the next API call
   nextFetch(originData: string[][], destinationData: any[], originIndex: number, destIndex: number, resolve: any){
     if(destIndex < destinationData.length - 1){
@@ -167,7 +250,7 @@ constructor() {
         this.fetchFlight(originData, destinationData, originIndex+1, 0, resolve);
       }
       else{
-        console.log("DONE")
+        console.log("DONE");
         resolve();
       }
     }
@@ -208,6 +291,16 @@ constructor() {
       this.origins.set('USA1', event.target.value/3);
       this.origins.set('USA2', event.target.value/3);
       this.origins.set('USA3', event.target.value/3);
+    }
+    else if(country == 'Canada'){
+      this.origins.set('Canada1', event.target.value/3);
+      this.origins.set('Canada2', event.target.value/3);
+      this.origins.set('Canada3', event.target.value/3);
+    }
+    else if(country == 'China'){
+      this.origins.set('China1', event.target.value/3);
+      this.origins.set('China2', event.target.value/3);
+      this.origins.set('China3', event.target.value/3);
     }
   }
 
